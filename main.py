1c09cdd945cd7ba68e9443f204649723bb460e6a
laurensart <artoflaurens@gmail.com>
2023-11-29 16:05:57 +0100

    finished

--- main.py
+++ main.py
@@ -1,49 +1,38 @@
 import pygame
 import math
 
-# Initialize Pygame
 pygame.init()
 
-# Set up the display
 width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Spiral Circle")
 
-# Colors
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-# Spiral parameters
 angle = 0
 radius = 0
 center_x, center_y = width // 2, height // 2
 speed = 0.1
 radius_increase = 0.5
-directionVar = 1
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-# Main loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # Fill the screen
     screen.fill(black)
 
-    # Calculate new position
     x = center_x + int(math.cos(angle) * radius)
     y = center_y + int(math.sin(angle) * radius)
 
-    # Check for horizontal boundaries and reverse direction if needed
-    if player_pos.x > screen.get_width()//2 - 15 or player_pos.x < 15:
-        directionVar = -directionVar
+    pygame.draw.circle(screen, white, (x, y), 15)
+    pygame.draw.line(screen, white, (center_x, center_y), (x, y))
 
-    # Update the position of the circle
-    player_pos = pygame.Vector2(x, y)
-    pygame.draw.circle(screen, white, player_pos, 15)
+    # Check for boundary and reverse direction if needed
+    if radius > screen.get_height() // 2 or radius < 0:
+        radius_increase = -radius_increase
 
     # Update the angle and radius
     angle += speed
